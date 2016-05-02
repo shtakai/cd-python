cddd4d2b00a9f588012d4516ce3b93f9a4020460
Alyson Takai <shtakai@gmail.com>
2016-05-02 15:42:22 -0700

    finished assignment multiples

--- 0502-am/assignment/multiples.py
+++ 0502-am/assignment/multiples.py
@@ -1,3 +1,16 @@
+# Part I
+#
+# Create a program that prints all the odd numbers from 1 to 1000. Use the for loop and don't use any array to do this exercise.
+
 for value in range(1,1000):
-    if value % 2 == 0:
+    if value % 2 != 0:
+        print value
+
+
+# Part II
+
+# Create another program that prints all the multiples of 5 from 5 to 1,000,000.
+
+for value in range(5,10000000):
+    if value % 5 == 0:
         print value
