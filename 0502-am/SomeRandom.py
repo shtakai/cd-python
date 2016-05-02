d1f4900d598d016f7c5a1a650fe3f1af5f48c90e
Alyson Takai <shtakai@gmail.com>
2016-05-02 10:48:51 -0700

    modified

--- 0502-am/SomeRandom.py
+++ 0502-am/SomeRandom.py
@@ -1,4 +1,5 @@
-print ("hello world")
+
+# print ("hello world")
 
 myVariable = ["phrase1", "phrase2", 37]
 
@@ -6,6 +7,17 @@ myVariable[0] = "new phrase"
 myVariable2 = ("banana", "flower", 42)
 myDecisionMaker = ("decision1","decision2")
 # True:1 False:0
-print myDecisionMaker[1==1]
+
+#dictionary
+myDictionary = {
+    "cat": "harry mammals that make me sneeze!",
+    "dog": "test"
+    }
+myDictionary["fish"] = "sashimi"
+
+print myDictionary["fish"]
+
+# print myDecisionMaker[1==1]
 for value in myVariable2:
-    print value
+    # print value
+    pass
