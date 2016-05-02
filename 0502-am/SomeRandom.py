735e810bf9371935c8cc72b912561dc62e32eb0e
Alyson Takai <shtakai@gmail.com>
2016-05-02 10:24:21 -0700

    tuples

--- 0502-am/SomeRandom.py
+++ 0502-am/SomeRandom.py
@@ -1,5 +1,11 @@
 print ("hello world")
 
 myVariable = ["phrase1", "phrase2", 37]
-for value in myVariable:
+
+myVariable[0] = "new phrase"
+#タプル　変更できない
+myVariable2 = ("banana", "flower", 42)
+#動かない
+# myVariable2[0] = "sashimi"
+for value in myVariable2:
     print value
