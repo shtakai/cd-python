a17f0c07ed67aedc383ee0b44d5f5914b3430310
Alyson Takai <shtakai@gmail.com>
2016-05-02 10:59:29 -0700

    modified

--- 0502-am/SomeRandom.py
+++ 0502-am/SomeRandom.py
@@ -14,6 +14,7 @@ myDictionary = {
     "dog": "test"
     }
 myDictionary["fish"] = "sashimi"
+myDictionary["human"] = "monkey sees mokeys do monkey has no iq copycat"
 
 print myDictionary["fish"]
 
