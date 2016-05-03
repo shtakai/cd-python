ec9ef8cf7e26531c68a236cfc4ddff6014372173
Alyson Takai <shtakai@gmail.com>
2016-05-03 16:40:17 -0700

    finished sessions

--- flask/assignment/form_test/server.py
+++ flask/assignment/form_test/server.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = "ThisIsSecret"
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/users', methods = ['POST'])
+
+@app.route('/users', methods=['POST'])
 def create_user():
     print "got post info"
-    name = request.form['name']
-    email = request.form['email']
-    return redirect('/')
+    session['name'] = request.form['name']
+    session['email'] = request.form['email']
+    return redirect('/show')
 
-app.run(debug = True)
+
+@app.route('/show')
+def show_user():
+    return render_template(
+        'user.html',
+        name=session['name'],
+        email=session['email']
+    )
+
+
+app.run(debug=True)
