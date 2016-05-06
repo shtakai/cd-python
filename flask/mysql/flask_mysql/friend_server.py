b1fbfc90b9f6a231e68c174794dd22d0962a38c1
Alyson Takai <shtakai@gmail.com>
2016-05-06 10:35:00 -0700

    modified before insertion (just show form only)

--- flask/mysql/flask_mysql/friend_server.py
+++ flask/mysql/flask_mysql/friend_server.py
@@ -9,13 +9,21 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/friends/<friend_id>', methods=['POST'])
-def create(friend_id):
+@app.route('/friends/<friend_id>')
+def index_friend(friend_id):
     query = "select * from friends where id= :specific_id"
-    specific_id = {'specific_id': friend_id}
-    friends = mysql.query_db("select * from friends where id= :specific_id")
-    # print friends
-    return render_template('index.html', all_friends=friends)
+    data= {'specific_id': friend_id}
+    friends = mysql.query_db(query, data)
+    print friends
+    return render_template('index.html', all_friends=friends[0])
+
+
+@app.route('/friends', methods=['POST'])
+def create():
+    print request.form['first_name']
+    print request.form['last_name']
+    print request.form['occupation']
+    return redirect('/')
 
 
 app.run(debug=True)
