4487cd5040a7b8c71518899f020872196d68401c
Alyson Takai <shtakai@gmail.com>
2016-05-06 08:02:53 -0700

    wip

--- flask/mysql/flask_mysql/shuffle_water/sf.rb
+++ flask/mysql/flask_mysql/shuffle_water/sf.rb
@@ -1,3 +1,4 @@
+require 'pp'
 
 def shuffle(l)
   return l[0] if l.size == 1
@@ -8,17 +9,21 @@ def shuffle(l)
 end
 
 
-def fill_water(l)
-  first = max(l)
-  index_first = l.delete_at(first)
-  second = max(l)
-  index_second = l.delete_at(second)
-  [head_arr, last_arr] = index_first > index_second ? [l[0..index_second], l[index_second..]] : []
-  
+def fill_water(list)
+  #pp l
+  first = list.max
+  index_first= list.index(first)
+  tmp_l = (list[0...index_first]  <<  list.min-1) + list[index_first...list.size-1]
+  pp tmp_l
+  second = tmp_l.max
+  index_second = tmp_l.index(second)
+  pp "first:#{first}:#{index_first}, second:#{second}:#{index_second}"
+  h,m,l = list[(0..(index_second-1))], list[((index_second-1)..index_first-1)], list[((index_first-1)..(list.size-1))] if index_first > index_second
+  pp h,m,l
 end
 
 
-
 array = [1,2,3,4,5,6]
-p shuffle(array)
+#fill_water( shuffle(array))
+fill_water( array)
 
