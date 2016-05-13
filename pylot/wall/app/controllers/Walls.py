779d207fc9e11d7c4893ba05e820770ebce88b5c
Alyson Takai <shtakai@gmail.com>
2016-05-13 11:00:28 -0700

    added template of wall

---
+++ pylot/wall/app/controllers/Walls.py
@@ -0,0 +1,32 @@
+"""
+    Sample Controller File
+
+    A Controller should be in charge of responding to a request.
+    Load models to interact with the database and load views to render them to the client.
+
+    Create a controller using this template
+"""
+from system.core.controller import *
+from time import strftime
+import datetime
+import string
+import random
+
+class Walls(Controller):
+    def __init__(self, action):
+        super(Walls, self).__init__(action)
+        """
+            This is an example of loading a model.
+            Every controller has access to the load_model method.
+
+            self.load_model('WelcomeModel')
+        """
+
+    """ This is an example of a controller method that will load a view for the client """
+    def index(self):
+        """
+        A loaded model is accessible through the models attribute
+        self.models['WelcomeModel'].get_all_users()
+        """
+        return self.load_view('index.html')
+
