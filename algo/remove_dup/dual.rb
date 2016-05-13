6c1d3ae99033e72a0b7651011520eacb7c148f00
Alyson Takai <shtakai@gmail.com>
2016-05-12 18:09:21 -0700

    implemented dual

--- algo/remove_dup/dual.rb
+++ algo/remove_dup/dual.rb
@@ -1,16 +1,15 @@
 require 'pp'
 
 def dual (a)
-  pp a
-  return nil if !a.any?
-  return a[0] if a.size == 1
-  head = [a[0], a[1]]
-  pp "get head #{head}"
+  return [] if !a.any?
+  return [a[0]] if a.size == 1
+  head = [a[1], a[0]]
   tail = a[2..a.size]
-  pp "tail: #{tail}"
-  head << dual(tail)
+  (head << dual(tail)).compact.flatten
 end
 
-
-arr = [1,2,3,4,5,6]
-dual arr
+(0..10).to_a.each{ |n|
+  arr = (0..n).to_a
+  pp "#{arr} -> #{dual arr}"
+}
+pp "#{[]} -> #{dual []}"
