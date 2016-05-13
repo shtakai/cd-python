1ff45fb2ab32eb47a81fbb1b472398d13c726617
Alyson Takai <shtakai@gmail.com>
2016-05-12 22:46:27 -0700

    wip

--- algo/remove_dup/rem.rb
+++ algo/remove_dup/rem.rb
@@ -1,20 +1,52 @@
 require 'pp'
 
 def shift_a(a)
-  pp "size: #{a.size}"
+  # move 1nd elem of array to next
+  #  next next....
+  # then 1nd elem moves to last of the array
+
+  pp "start #{a}"
+  head = a[0]
+  tail = a[1..a.size]
+
+  # Basic condition
+  return [] if !tail.any?
+
+
+  # Recursion
+  head, tail[0] = tail[0], head
+  ([head] << shift_a(tail)).flatten
+
+end
+
+def remove_dup(a)
   return [a[0]] if a.size == 1
-  return [a[1],a[0]] if a.size == 2
-  return [] if !a.any?
-  pp a
-  head = a[1]
-  tail = ([a[0]] << a[2..a.size]).flatten
-  pp "head:#{head}. tail:#{tail}"
-  (head << (shift_a tail)).compact.flatten
+  head = a[0]
+  tail = a[1..a.size]
+
+  pp "head:#{head}  tail:#{tail}"
+
+  if head == tail[0]
+    [head] << shift_a(tail)
+  else
+    [head] << remove_dup(tail)
+  end
+
 end
 
 
-
 arr = [1,2,2,3,4,5,5,6]
+# 1 2 2 3 4
+# 1 2
+#   2 2
+#    <2<3<
+#     3 2 4
+#      <2<4<
+#       4<2
+#         X
+# 1 2 3 4 _
+#
 #arr = [1,2]
-#pp remove_dup arr
-pp shift_a arr
+pp remove_dup arr
+#pp shift_a shift_a shift_a arr
+#pp  shift_a arr
