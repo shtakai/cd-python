72fc0e9dfa78908dafbcb88f0ca2603649c64cd1
Alyson Takai <shtakai@gmail.com>
2016-05-12 18:09:40 -0700

    WIP remove_dup

--- algo/remove_dup/rem.rb
+++ algo/remove_dup/rem.rb
@@ -1,41 +1,20 @@
 require 'pp'
 
-
-
-def remove_dup(a)
-  h = a[0]
-  t = a[1..a.size]
-  pp "H:#{h},  T:#{t}"
-  if h == t[0]
-    pp "dup"
-    shift_a t
-  else
-    pp "not dup"
-    pp "next target: #{t}"
-  end
-  remove_dup t
-  nil
-end
-
 def shift_a(a)
-  pp "----"
-  return a[0] if a.size == 1
-  h = a[0]
-  t = a[1..a.size]
-  pp "h:#{h}  t:#{t}"
-  pp "h:#{h} t0:#{t[0]}"
-  if h == t[0]
-    pp "Dup"
-    h, t[0] = t[0], h
-    pp "h:#{h} t0:#{t[0]}"
-  else
-    pp "No Dup"
-  end
-  shift_a(t)
-
+  pp "size: #{a.size}"
+  return [a[0]] if a.size == 1
+  return [a[1],a[0]] if a.size == 2
+  return [] if !a.any?
+  pp a
+  head = a[1]
+  tail = ([a[0]] << a[2..a.size]).flatten
+  pp "head:#{head}. tail:#{tail}"
+  (head << (shift_a tail)).compact.flatten
 end
+
 
 
 arr = [1,2,2,3,4,5,5,6]
+#arr = [1,2]
 #pp remove_dup arr
 pp shift_a arr
